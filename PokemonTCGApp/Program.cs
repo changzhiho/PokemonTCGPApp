54fdcfcddb745a8794fd1e65ad115abb8dd0f126
changzhiho <changzhiho@hotmail.com>
2025-02-15 11:07:24 +0100

    Initialization of Program.cs

--- PokemonTCGApp/Program.cs
+++ PokemonTCGApp/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using PokemonTCGApp;
+using PokemonTCGBlazor;
+using PokemonTCGBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<PokemonService>();
+builder.Services.AddScoped<FavortiesService>();
 
 await builder.Build().RunAsync();
