6035e942f32af6dcdf56f824eed90a540b76f7fc
changzhiho <changzhiho@hotmail.com>
2025-02-22 10:54:56 +0100

    Changement de favoritesServices en async

--- PokemonTCGApp/Program.cs
+++ PokemonTCGApp/Program.cs
@@ -7,8 +7,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Enregistrement des services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<PokemonService>();
 builder.Services.AddScoped<FavoritesService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+// Récupération de FavoritesService après la construction de l'application
+var favoritesService = host.Services.GetRequiredService<FavoritesService>();
+await favoritesService.InitializeAsync(); // Chargement des favoris
+
+await host.RunAsync();
